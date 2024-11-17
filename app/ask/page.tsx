c9e2bf22fb71285abe56e8a868d324cb93736f56
Johannes Mario Meissner <jmariomeissner@gmail.com>
2024-11-17 13:38:17 +0900

    Bump

--- app/ask/page.tsx
+++ app/ask/page.tsx
@@ -4,7 +4,7 @@ import ChatPage from "@/components/chat";
 import React from "react";
 
 const mockInput =
-  'Hi, \nI\'ve received the following email. What should I do? \n\n"I’m checking that everything is OK for the next shipment.\nI hope there won’t be any problems with box sizes."';
+  'Hi, \nI\'ve received the following email. What should I do?\n\n"I’m checking that everything is OK for the next shipment.\nI hope there won’t be any problems with box sizes."';
 
 export default function AskPage() {
   return (
